bec6e5f29fb4f0836cd0bef68f679e58153fa4cf
Zhi <zhichen0428@gmail.com>
2022-04-25 14:22:59 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,5 +62,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
- breathe_projects = {"advection": "../doxy_files/xml" }
- breathe_default_project = "advection"
+breathe_projects = {"advection": "../doxy_files/xml" }
+breathe_default_project = "advection"
